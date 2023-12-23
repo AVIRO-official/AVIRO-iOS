0000fb9dc9ab5b41169836ca8856e682d898eb54
Jeon Seoung Hun <jsh097610@gmail.com>
2023-12-24 00:42:04 +0900

    Review UI 구현 완료

--- AVIRO/Scene/TabBar/Home/ReviewWriteView/ViewController/ReviewWriteViewController.swift
+++ AVIRO/Scene/TabBar/Home/ReviewWriteView/ViewController/ReviewWriteViewController.swift
@@ -5,7 +5,6 @@
 //  Created by 전성훈 on 2023/12/19.
 //
 
-
 // MARK: Count 바인딩, textview text 바인딩, 등록 바인딩
 import RxSwift
 import RxCocoa
@@ -13,7 +12,7 @@ import RxCocoa
 final class ReviewWriteViewController: UIViewController {
     private var viewModel: ReviewWriteViewModel!
     private var disposeBag = DisposeBag()
-    
+        
     private lazy var placeInfoView: ReviewPlaceInfoView = {
         let view = ReviewPlaceInfoView()
         
@@ -47,7 +46,7 @@ final class ReviewWriteViewController: UIViewController {
     private lazy var placeholderLabel: UILabel = {
         let label = UILabel()
         
-        label.text = "욕설, 비방 등 사장님과 다른 사용자들을 불쾌하게 하는 내용은 남기지 말아주세요."
+        label.text = "사장님과 다른 사용자들이 상처받지 않도록 좋은 표현을 사용해주세요."
         label.textColor = .gray5
         label.lineBreakMode = .byCharWrapping
         label.numberOfLines = 0
@@ -66,7 +65,7 @@ final class ReviewWriteViewController: UIViewController {
     private lazy var exampleLabel: UILabel = {
         let label = UILabel()
         
-        label.text = "맛, 가격, 분위기, 편의시설, 비건프렌들리함 등"
+        label.text = "도움이 돼요 : 맛, 가격, 분위기, 편의시설, 비건프렌들리함 등"
         label.textColor = .gray2
         label.numberOfLines = 1
         label.font = .pretendard(size: 16, weight: .medium)
@@ -85,13 +84,16 @@ final class ReviewWriteViewController: UIViewController {
     private lazy var reviewUploadButton: UIButton = {
         let button = UIButton()
         
-        button.backgroundColor = .keywordBlue
         button.setTitle("후기 등록하기", for: .normal)
         button.titleLabel?.font = .pretendard(size: 17, weight: .semibold)
         button.setTitleColor(.gray7, for: .normal)
         
+        button.setTitleColor(.gray2, for: .disabled)
         return button
     }()
+        
+    private var reviewTextViewWhenKeyboardWillShow: NSLayoutConstraint!
+    private var reviewTextViewWhenKeyboardWillHide: NSLayoutConstraint!
     
     private var afterViewDidLoad = false
     
@@ -140,7 +142,6 @@ final class ReviewWriteViewController: UIViewController {
         }
         
         NSLayoutConstraint.activate([
-            // TODO: placeInfoVIew 높이는 값에 따라 동적으로 다르게 설정
             placeInfoView.topAnchor.constraint(
                 equalTo: self.view.safeAreaLayoutGuide.topAnchor,
                 constant: 20
@@ -215,7 +216,19 @@ final class ReviewWriteViewController: UIViewController {
             reviewUploadButton.trailingAnchor.constraint(
                 equalTo: self.view.trailingAnchor
             )
+            
         ])
+        
+        reviewTextViewWhenKeyboardWillShow = self.reviewTextView.topAnchor.constraint(
+            equalTo: self.view.safeAreaLayoutGuide.topAnchor
+        )
+        reviewTextViewWhenKeyboardWillHide = self.reviewTextView.topAnchor.constraint(
+            equalTo: self.placeInfoView.bottomAnchor,
+            constant: 20
+        )
+        
+        reviewTextViewWhenKeyboardWillShow.isActive = false
+        reviewTextViewWhenKeyboardWillHide.isActive = true
         
         UIView.animate(withDuration: 0.5, delay: 0, options: [.repeat, .autoreverse]) {
             self.exampleSticy.transform = CGAffineTransform(translationX: 0, y: 3)
@@ -223,6 +236,14 @@ final class ReviewWriteViewController: UIViewController {
     }
     
     private func setupAttribute() {
+        let tapGesture = UITapGestureRecognizer()
+
+        tapGesture.rx.event.bind { [weak self] _ in
+            self?.view.endEditing(true)
+        }.disposed(by: disposeBag)
+
+        view.addGestureRecognizer(tapGesture)
+        
         navigationController?.navigationBar.isHidden = false
 
         self.view.backgroundColor = .gray7
@@ -250,11 +271,9 @@ final class ReviewWriteViewController: UIViewController {
     }
     
     private func dataBinding() {
-        let textViewBiginEditing = reviewTextView.rx.didBeginEditing.asDriver()
         let text = reviewTextView.rx.text.asDriver()
         
         let input = ReviewWriteViewModel.Input(
-            textViewBiginEditing: textViewBiginEditing,
             text: text
         )
         
@@ -268,18 +287,59 @@ final class ReviewWriteViewController: UIViewController {
             .drive(self.rx.textCountUpdate)
             .disposed(by: disposeBag)
         
-        // TODO: 쉐이크 애니메이션
         output.review
             .drive(reviewTextView.rx.text)
             .disposed(by: disposeBag)
+        
+        output.keyboardWillShow
+            .drive(self.rx.keyboardWillShow)
+            .disposed(by: disposeBag)
+
+        output.keyboardWillHide
+            .drive(self.rx.keyboardWillHide)
+            .disposed(by: disposeBag)
+    }
+    
+    func adjustViewForKeyboardWillShow() {
+        UIView.animate(withDuration: 0.3) {
+            self.reviewTextViewWhenKeyboardWillHide.isActive = false
+            self.reviewTextViewWhenKeyboardWillShow.isActive = true
+
+            self.view.layoutIfNeeded()
+        }
+    }
+
+    func adjustViewForKeyboardWillHide() {
+        UIView.animate(withDuration: 0.3) {
+            self.reviewTextViewWhenKeyboardWillShow.isActive = false
+            self.reviewTextViewWhenKeyboardWillHide.isActive = true
+
+            self.view.layoutIfNeeded()
+        }
     }
     
     func updateTextViewPlaceHolder(with isEditing: Bool) {
         placeholderLabel.isHidden = isEditing
     }
     
-    func updateTextCount(with textCount: String) {
-        let coloredRange = NSRange(location: 0, length: textCount.count)
+    func updateTextCount(with textCount: Int) {
+        updateButtonEnable(with: textCount)
+        updateTextViewCountLabel(with: textCount)
+        
+        if textCount == 200 {
+            whenOverText()
+        }
+    }
+    
+    private func updateButtonEnable(with textCount: Int) {
+        reviewUploadButton.isEnabled = textCount != 0
+        reviewUploadButton.backgroundColor = textCount != 0 ? .keywordBlue : .gray5
+    }
+    
+    private func updateTextViewCountLabel(with textCount: Int) {
+        let textCountString = String(textCount)
+        
+        let coloredRange = NSRange(location: 0, length: textCountString.count)
         
         let coloredAttributes = [
             NSAttributedString.Key.foregroundColor: UIColor.keywordBlue,
@@ -293,9 +353,9 @@ final class ReviewWriteViewController: UIViewController {
         
         let attributedText = NSMutableAttributedString(
             string: [
-                textCount,
+                textCountString,
                 "/",
-                "255"
+                "200"
             ].joined()
         )
         
@@ -306,12 +366,16 @@ final class ReviewWriteViewController: UIViewController {
         attributedText.addAttributes(
             grayAttributes,
             range: NSRange(
-                location: textCount.count,
-                length: attributedText.length - textCount.count
+                location: textCountString.count,
+                length: attributedText.length - textCountString.count
             )
         )
         
         textViewCountLabel.attributedText = attributedText
+    }
+    
+    private func whenOverText() {
+        reviewTextView.activeHshakeEffect()
     }
 }
 
@@ -322,9 +386,22 @@ extension Reactive where Base: ReviewWriteViewController {
         }
     }
     
-    var textCountUpdate: Binder<String> {
+    var textCountUpdate: Binder<Int> {
         return Binder(self.base) { base, textCount in
             base.updateTextCount(with: textCount)
         }
+    }
+    
+    var keyboardWillShow: Binder<Void> {
+        return Binder(self.base) { base, _ in
+            base.adjustViewForKeyboardWillShow()
+        }
+    }
+    
+    var keyboardWillHide: Binder<Void> {
+        return Binder(self.base) { base, _ in
+            base.adjustViewForKeyboardWillHide()
+        }
+
     }
 }
--- AVIRO/Scene/TabBar/Home/ReviewWriteView/ViewModel/ReviewWriteViewModel.swift
+++ AVIRO/Scene/TabBar/Home/ReviewWriteView/ViewModel/ReviewWriteViewModel.swift
@@ -8,6 +8,7 @@ import RxSwift
 import RxCocoa
 
 final class ReviewWriteViewModel: ViewModel {
+    private let disposeBag = DisposeBag()
     
     var placeIcon: UIImage!
     var placeTitle: String!
@@ -21,42 +22,56 @@ final class ReviewWriteViewModel: ViewModel {
         self.placeIcon = placeIcon
         self.placeTitle = placeTitle
         self.placeAddress = placeAddress
+        
     }
     
     struct Input {
-        let textViewBiginEditing: Driver<Void>
         let text: Driver<String?>
     }
     
     struct Output {
         let isEditing: Driver<Bool>
-        let textCount: Driver<String>
+        let textCount: Driver<Int>
         let review: Driver<String>
+        let keyboardWillShow: Driver<Void>
+        let keyboardWillHide: Driver<Void>
     }
     
     func transform(with input: Input) -> Output {
-        let isEditing = input.textViewBiginEditing
-            .map { true }
+        let isEditing = input.text.map {
+            $0?.count ?? 0 > 0 ? true : false
+        }
         
         let restrictedText = input.text
-            .flatMapLatest { text -> Driver<String> in
+            .map { text in
                 let text = text ?? ""
-                if text.count > 255 {
-                    let index = text.index(text.startIndex, offsetBy: 255)
+                if text.count > 200 {
+                    let index = text.index(text.startIndex, offsetBy: 200)
                     let restricted = String(text[..<index])
-                    return Driver.just(restricted)
+                    return restricted
                 } else {
-                    return Driver.just(text)
+                    return text
                 }
             }
         
         let textCount = restrictedText
-            .map { "\($0.count)" }
+            .map { $0.count }
+        
+        let keyboardWillShow = NotificationCenter.default.rx.notification(UIResponder.keyboardWillShowNotification)
+            .map { _ in }
+            .asDriver(onErrorDriveWith: .empty())
+
+        let keyboardWillHide = NotificationCenter.default.rx.notification(UIResponder.keyboardWillHideNotification)
+            .map { _ in }
+            .asDriver(onErrorDriveWith: .empty())
         
         return Output(
             isEditing: isEditing,
             textCount: textCount,
-            review: restrictedText
+            review: restrictedText,
+            keyboardWillShow: keyboardWillShow,
+            keyboardWillHide: keyboardWillHide
         )
     }
+    
 }
